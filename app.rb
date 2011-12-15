283a8cd5239d9e194aeb42470eeb5c3806606db3
geekontheway <zxl51@qq.com>
2011-12-15 15:47:50 +0800

    ...

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+set :public_folder, File.dirname(__FILE__)
+
+get '/' do
+  'Hello world! mos'
+end
+
+post '/test' do
+  system("cd #{File.dirname(__FILE__)}/emall; touch sinatratest.md ")
+  'Test if sinatra works' 
+end
