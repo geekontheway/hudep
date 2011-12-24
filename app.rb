b46c46f8574e9a3e0436e6b4be18bcfbcca3221c
geekontheway <zxl51@qq.com>
2011-12-24 15:39:30 +0800

    修正get…end语法错误，添加几个route

--- app.rb
+++ app.rb
@@ -1,19 +1,39 @@
-
 require 'sinatra'
+
+#mail
+require 'pony'
+
 
 set :public_folder, File.dirname(__FILE__)
 
 get '/' do
   'Hello world! mos'
-end
-get '/another' do
-  'Another Hello world!'
+   send_file('panda.png')
+# blow files will not be sent.
+   send_file('ruboto.png')
 end
 post '/test' do
-system("cd #{File.dirname(__FILE__)}; date >> test.log")
+  system("cd #{File.dirname(__FILE__)}; date >> test.log")
 end
-
 post '/stagingdeploy' do
   system("cd #{File.dirname(__FILE__)+"/../emall"};cap staging deploy:setup;cap staging deploy:cold;cap staging deploy")
-  'Cap staging deploy using capistrano' 
+  'Cap staging deploy using capistrano'
 end
+
+
+#some test
+get '/send_file' do
+  send_file('panda.png')
+end
+get '/attachment' do
+  attachment "panda.png"
+  "store it!"
+end
+get '/sinatra' do
+redirect 'http://www.sinatrarb.com/'
+end
+
+
+
+
+
